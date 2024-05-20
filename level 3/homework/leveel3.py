20fdda5cf64d3e1ed3f35458d84bf7875c2ab26f
barbarejavakhadze01 <barbarejava@gmail.com>
2024-05-20 18:58:38 +0400

    level3

---
+++ level 3/homework/leveel3.py
@@ -0,0 +1,7 @@
+number = 23
+
+number1 = 34
+
+number2 = 10
+
+print(number + number1 + number2)
---
+++ level 3/homework/level3.py
@@ -0,0 +1,11 @@
+name = "barbare"
+
+surname = "javakhadze"
+
+age = 12
+
+hobby = "programming"
+
+country = "georgia"
+
+print(name,surname,age,hobby,country)
