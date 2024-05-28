313912c89c3d9e67ab7393692574b7a89e901c01
barbarejavakhadze01 <barbarejava@gmail.com>
2024-05-28 18:09:24 +0400

    homework

---
+++ level 05/homework/variable.py
@@ -0,0 +1,20 @@
+name = "barbare"
+hobby = "programming"
+favorite_subjects = "science and math"
+pet = "dog"
+its_name = "Molli"
+my_age = 12
+height = 155
+place_of_residence = "nutsubidze plato"
+country = "Georgia"
+city = "Tbilisi"
+
+print(name,hobby,favorite_subjects, pet, its_name, my_age , height, place_of_residence, country, city )
+
+# my name is Barbare
+# my hobby is programming, it's very interesting for me and i like it
+# my favorite subjects are science and math, there are interesting and i learn a lot from them
+# i have a pet, dog, cocker spaniel. its name is Molli. she is very cute and loves to eat and play
+# i am 12 years old, i was born in 2012 in january 6th
+# my height is 155
+# i live in Georgia, Tbilisi. my adressis nutsubidze plato 4
