9a950c551db0482d667e2af3d52dd326d7cfa89e
barbarejavakhadze01 <barbarejava@gmail.com>
2024-06-03 22:48:56 +0400

    classwork

---
+++ level 07/classwork/classwork1.py
@@ -0,0 +1,19 @@
+name = 'barbare'
+Name = 'lasha'
+NAME = 'dato'
+
+print(name)
+
+ #python არის ისეთი ენა, რომ დიდი მნიშვნელობა აქვს დიდი და პატარა ასოთი დაწერას
+
+name = 'barbare'
+Name = 'moli'
+nAme = 'GOA'
+
+print(name , Name , nAme)
+
+salary = 150000
+pay_raise = 10000
+new_salary = salary + pay_raise
+print(new_salary)
+
---
+++ level 07/classwork/classwork2.py
@@ -0,0 +1,37 @@
+name = input("what is your name: ")
+last_name = input("what is your last name: ")
+age = input("what is your age: ")
+country = input("which country do you live: ")
+
+print ( 'My name is ', name, last_name, 'i am ', age, ' years old' ' and i live in ', country)
+
+age = input('enter your age: ')
+print(type(age))
+
+x = int(input( 'enter X; '))
+y = int(input( 'enter Y: '))
+
+print(x + y)
+
+str()
+int()
+
+x = '910'
+print(type(int(x)))
+
+name = 'gega'
+age = 14
+salary = 3.5
+
+print(type(name))
+print(type(age))
+print(type(salary))
+
+
+
+
+
+
+
+
+
