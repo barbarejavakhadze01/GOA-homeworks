1bd117fb3057aab0685c9224921d23e75e9b4e95
barbarejavakhadze01 <barbarejava@gmail.com>
2024-06-18 17:23:57 +0400

    homework

---
+++ level 11/classwork/input.py
@@ -0,0 +1,24 @@
+savings = input()
+ 
+updSavings = float(savings)
+
+balance = updSavings * 5
+
+balance = str(balance)
+
+message = 'Amount in 1 year: ' + balance
+
+print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 11/homework/programm.py
@@ -0,0 +1,82 @@
+#1) გააკეთეთ 30 მაგალითი შედარებით ოპერატორებზე. 
+print(80 > 39)
+
+print(100 > 45)
+
+print(45 == 45)
+
+print(98 != 78)
+
+print(30 > 21)
+
+print(768 > 987)
+
+print(67 > 98)
+
+print(98 < 100)
+      
+print(390 > 13)
+
+print(868 < 22)
+
+print(765 > 675)
+
+print(100 == 100)
+
+print(567 > 567)
+
+print(89 < 98)
+
+print(253 == 98)
+
+print(70 > 80)
+
+print(56 < 76)
+
+print(87 > 37)
+
+print(987 < 789)
+
+print(111 != 111)
+
+print(364 > 463)
+
+print(89 == 89)
+
+print(67 < 76)
+
+print(89 > 98)
+
+print(450 == 445)
+
+print(67 == 76)
+
+print(39 < 98)
+
+print(25 < 25)
+
+print(98 == 98)
+
+
+
+
+
+#2) დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი.
+
+person1 = input("enter first person age: ")
+person2 = input("enter second person age: ")
+
+print(person1 > person2)
+
+print(person1 < person2)
+
+print(person1 == person2)
+
+
+
+
+
+
+
+
+
