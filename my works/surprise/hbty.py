ec66398d3e03cae5c8f7150866bb02cff1fc26d4
barbarejavakhadze01 <barbarejava@gmail.com>
2024-06-23 14:12:49 +0400

    presents

---
+++ my works/surprise/hbty.py
@@ -0,0 +1,9 @@
+# Happy Birthday Lasha!!
+
+Lider = input("enter your lider of the squad;")
+
+oh = input("oh,is he Lasha Dekanoidze?")
+
+print("so today is his birthday!! Let's congratulate him")
+
+print("this is present for you!! your portret)) you can see it in file portret.py")
