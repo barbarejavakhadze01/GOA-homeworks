feb3c94957a0b629b9347025b9578b1054f51f85
barbarejavakhadze01 <barbarejava@gmail.com>
2024-06-30 20:50:31 +0400

    classwork

---
+++ level 13/homework/check.py
@@ -0,0 +1,24 @@
+# and
+a = 97
+print(a>7 and a<7)
+b = 45
+print(b>20 and b<34)
+c = 34
+print(c<60 and c>31) 
+d = 77
+print(d<78 and d<32)
+e = 89
+print(e>20 and e>80)
+
+# or
+
+f = 23
+print(f<23 or f<20)
+g = 67
+print(g<80 or 98>g)
+h = 95
+print(h>45 or 76>h)
+i = 0
+print(i<32 or 73<i)
+j = 23
+print(45<j or 78>j)
