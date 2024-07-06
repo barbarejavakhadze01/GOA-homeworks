dd8d558b6e61b3d7da4d6c219c83a82adc8a1704
barbarejavakhadze01 <barbarejava@gmail.com>
2024-07-06 21:48:11 +0400

    homework

---
+++ level 15/classwork/input2.py
@@ -0,0 +1,22 @@
+# დაწერეთ პროგრამა, რომელიც მოუწოდებს მომხმარებელს შეიყვანოს ნივთების რაოდენობა, რომლის შეძენაც სურს.დარწმუნდით, რომ მომხმარებელი შეიყვანს დადებითი რიცხვს.თითოეული ნივთისთვის, შესთავაზეთ მომხმარებელს შეიყვანოს ფასი.დარწმუნდით, რომ მომხმარებელი შეიყვანოს დადებითი რიცხვს პროდუქტის ფასისთვის.გამოთვალეთ და აჩვენეთ ყველა ელემენტის მთლიანი ღირებულება.
+
+product = int(input("how many products do you want to buy? "))
+which = input("which products do you want to buy? ")
+milk = 3
+bread = 2
+cheese = 23
+meet = 38
+all_products = milk + bread + cheese + meet
+all = input("all products cost; ")
+print(all_products)
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 15/homework/variable.py
@@ -0,0 +1,27 @@
+# შემოიტანეთ სხვადასხვა ცვლადები, რომლებსაც ექნებათ სხვადასხვა მონაცემთა ტიპი, შემდგომ შეამოწმეთ ეს ტიპები.
+
+type_0 = 23
+type_1 = 32.6
+type_2 = "GOA"
+type_3 = 8.9
+type_4 = "21"
+type_5 = 67
+type_6 = 397
+type_7 = 1.0
+type_8 = "43"
+type_9 = 92
+type_10 = 65.2
+type_11 = "academy"
+
+print(type(type_0))
+print(type(type_1))
+print(type(type_2))
+print(type(type_3))
+print(type(type_4))
+print(type(type_5))
+print(type(type_6))
+print(type(type_7))
+print(type(type_8))
+print(type(type_9))
+print(type(type_10))
+print(type(type_11))
