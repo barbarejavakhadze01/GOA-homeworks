4071e5fabf6c2a7f61aa41e7751a4841c870f191
barbarejavakhadze01 <barbarejava@gmail.com>
2024-07-25 15:17:15 +0400

    homework

---
+++ level 19/homework/forloop2.py
@@ -0,0 +1,22 @@
+# for loop - ის დახმარებით 1 - დან 47 - მდე გამოიტანეთ რიცხვების ჯამი, გამოიყენეთ ( + )ოპერატორი, ხოლო შემდეგ განახორციელეთ იგივე რამ, ( * ) - ოპერატორზე.
+
+# variant 1
+
+total = 0
+for i in range(1, 47):
+    total += i
+print('sum: ', total)
+
+
+
+# variant 2
+
+# + operator
+
+for i in range (1 , 47):
+    print(i + i)
+
+# * operator
+
+for i in range (1 , 47):
+    print(i * i)
