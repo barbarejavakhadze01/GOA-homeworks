ff99ebbf52d3c672e22afcb0c11a9c7e3d33bad1
barbarejavakhadze01 <barbarejava@gmail.com>
2024-07-28 23:32:19 +0400

    homework

---
+++ level 21/homework/homework.py
@@ -0,0 +1,5 @@
+text = str(input('enter text; '))
+guess_text = str(input('guess the text; '))
+
+while text != guess_text:
+    guess_again_number = str(input('guess again the text; '))
---
+++ level 21/homework/ifelse.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+
+if a > b:
+    print('a is greater that b')
+else:
+    print('b is greater that a')
