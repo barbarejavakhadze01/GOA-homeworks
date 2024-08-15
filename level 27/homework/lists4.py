039d549b849c6245a26c13bef1745ecbec341cac
barbarejavakhadze01 <barbarejava@gmail.com>
2024-08-15 16:48:57 +0400

    homework

---
+++ level 27/homework/lists4.py
@@ -0,0 +1,10 @@
+# 5) სიის ელემენტების გამრავლება
+# შექმენი სია რიცხვებით და დაწერე კოდი, რომელიც გამოითვლის სიის ყველა ელემენტის ნამრავლს და დაბეჭდავს შედეგს.
+
+numbers = [1, 2, 3, 4, 5]
+
+multiplication = [numbers[0] * numbers[0], numbers[1] * numbers[1], numbers[2] *numbers[2], numbers[3] * numbers[3], numbers[4] * numbers[4]]
+
+print(multiplication)
+
+
